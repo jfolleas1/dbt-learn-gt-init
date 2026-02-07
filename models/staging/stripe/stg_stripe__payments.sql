7b05fccbe9a2a40980dd561fedcf0b7bf5748ff4
Jacques Folleas <jacques.folleas.pro@gmail.com>
2026-02-07 14:44:27 +0100

    add codegen package and used it to generate soruces and staging models

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,9 +1,22 @@
-SELECT 
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    amount,
-    created as created_at
-FROM
-    raw.stripe.payment
+with source as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+renamed as (
+
+    select
+        id,
+        orderid,
+        paymentmethod,
+        status,
+        amount,
+        created,
+        _batched_at
+
+    from source
+
+)
+
+select * from renamed
